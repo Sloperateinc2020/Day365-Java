3673c96e197e23b02985ce5bb60214ccda5d7d64
RNBPRA <bhavaniprasadramisetti999@gmail.com>
2024-11-04 11:20:23 +0530

    Initial commit

---
+++ src/main/java/com/day365/online/Day365Application.java
@@ -0,0 +1,13 @@
+package com.day365.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day365Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day365Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/day365/online/Day365ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.day365.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day365ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
